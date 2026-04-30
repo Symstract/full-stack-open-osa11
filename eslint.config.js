1bc65a004a0f8400978422e96496fa47ecf1f932
Antti Tikka <antti.tikka@hotmail.com>
2026-04-30 19:23:44 +0300

    FIx linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,26 @@ module.exports = [
       'no-console': 0
     }
   },
+    {
+    files: ['playwright.config.js'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-console': 0
+    }
+  },
   {
     files: ['src/**/*.{js,jsx}', 'test/**/*.{js,jsx}'],
     plugins: {
